00fe04cccc0fdbd68c7e7975ab371d4dd4ff52d7
ArditBaloku <arditbaloku@gmail.com>
2023-07-30 17:09:24 +0200

    Add speedup logic

--- cpu.js
+++ cpu.js
@@ -9,6 +9,7 @@ async function computePassCpu() {
       let positionInWay = index;
       let previousNode = node;
       let nextNode = previousNode;
+      let canSpeedUp = false;
       for (let distanceToCheck = 0; distanceToCheck < Math.max(car.speed, 1); distanceToCheck++) {
         if (positionInWay + 1 > currentWay.nodes.length - 1) {
           currentWay = currentWay.connections[0];
@@ -24,6 +25,7 @@ async function computePassCpu() {
 
         if (isCarOnNextNode) {
           return {
+            id: car.id,
             lat: previousNode.lat,
             lon: previousNode.lon,
             wayId: previousNode.wayId,
@@ -34,14 +36,37 @@ async function computePassCpu() {
 
         previousNode = nextNode;
         positionInWay++;
+
+        // If on the last loop of the for loop, and the next node after that is open,
+        // and we have not reached max speed yet (2), then we can speed up
+        // Also ignoring cars with a speed of 0 because they are going to speed up anyway
+        if (distanceToCheck === Math.max(car.speed, 1) - 1 && car.speed < 2 && car.speed > 0) {
+          if (positionInWay + 1 > currentWay.nodes.length - 1) {
+            currentWay = currentWay.connections[0];
+            positionInWay = 0;
+
+            if (!currentWay) {
+              break;
+            }
+          }
+
+          nextNode = currentWay.nodes[positionInWay + 1];
+          const isCarOnNextNode = cars.find((x) => x.nodeId === nextNode.id);
+
+          if (!isCarOnNextNode) {
+            canSpeedUp = true;
+          }
+        }
       }
 
+      const speedUp = canSpeedUp ? 1 : 0;
       return {
+        id: car.id,
         lat: nextNode.lat,
         lon: nextNode.lon,
         wayId: nextNode.wayId,
         nodeId: nextNode.id,
-        speed: Math.max(car.speed, 1),
+        speed: Math.max(car.speed, 1) + speedUp,
       };
     })
     .filter(Boolean);
