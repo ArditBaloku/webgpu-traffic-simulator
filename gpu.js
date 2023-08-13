800b93da02bda63a284e17dae2d3be0de8b31f4b
ArditBaloku <arditbaloku@gmail.com>
2023-08-13 20:44:12 +0200

    Add existing logic to gpu

--- gpu.js
+++ gpu.js
@@ -39,6 +39,7 @@ function buildGpuWaysArray() {
   let connectionsOffset = 0;
   let nodesOffset = 0;
   const gpuWays = [];
+  const connectionIds = [];
 
   for (const way of ways) {
     const gpuWay = {
@@ -51,6 +52,7 @@ function buildGpuWaysArray() {
     };
 
     gpuWays.push(gpuWay);
+    connectionIds.push(...way.connections.map((x) => Number(x.id)));
     connectionsOffset += way.connections.length;
     nodesOffset += way.nodes.length;
   }
@@ -63,7 +65,7 @@ function buildGpuWaysArray() {
     way.nodesLength,
     way.isRoundabout,
   ]);
-  return new Uint32Array(arr);
+  return [new Uint32Array(arr), new Uint32Array(connectionIds)];
 }
 
 function buildGpuNodesArray() {
@@ -73,7 +75,37 @@ function buildGpuNodesArray() {
   return [nodeIdsArray, nodeCoordsArray];
 }
 
+function buildGpuCarsArray() {
+  const gpuCarsFloatArray = new Float32Array(cars.flatMap((x) => [x.lat, x.lon]));
+  const gpuCarsUintArray = new Uint32Array(cars.flatMap((x) => [x.id, x.wayId, x.nodeId, x.speed]));
+  return [gpuCarsFloatArray, gpuCarsUintArray];
+}
+
+function buildCpuCarsArray(carUintArray, carFloatArray) {
+  const cars = [];
+  let offset = 0;
+  for (let i = 0; i < carUintArray.length; i += 4) {
+    if (carUintArray[i] === 0) {
+      offset += 2;
+      continue;
+    }
+
+    cars.push({
+      id: carUintArray[i],
+      wayId: carUintArray[i + 1],
+      nodeId: carUintArray[i + 2],
+      speed: carUintArray[i + 3],
+      lat: carFloatArray[offset],
+      lon: carFloatArray[offset + 1],
+    });
+
+    offset += 2;
+  }
+  return cars;
+}
+
 waysBuffer = null;
+connectionIdsBuffer = null;
 nodeIdsBuffer = null;
 nodeCoordsBuffer = null;
 staticBuffersInitialized = false;
@@ -82,9 +114,13 @@ function initStaticBuffers() {
     return;
   }
 
-  const gpuWaysArray = buildGpuWaysArray();
+  const [gpuWaysArray, connectionIdsArray] = buildGpuWaysArray();
   waysBuffer = createGPUBuffer(
     gpuWaysArray,
+    GPUBufferUsage.STORAGE | GPUBufferUsage.READ | GPUBufferUsage.COPY_SRC
+  );
+  connectionIdsBuffer = createGPUBuffer(
+    connectionIdsArray,
     GPUBufferUsage.STORAGE | GPUBufferUsage.READ | GPUBufferUsage.COPY_SRC
   );
 
@@ -99,6 +135,31 @@ function initStaticBuffers() {
   );
 
   staticBuffersInitialized = true;
+}
+
+carsFloatBuffer = null;
+carsUintBuffer = null;
+carsFloatResultsBuffer = null;
+carsUintResultsBuffer = null;
+function initCarsBuffers() {
+  const [gpuCarsFloatArray, gpuCarsUintArray] = buildGpuCarsArray();
+  carsFloatBuffer = createGPUBuffer(
+    gpuCarsFloatArray,
+    GPUBufferUsage.STORAGE | GPUBufferUsage.READ | GPUBufferUsage.COPY_SRC
+  );
+  carsUintBuffer = createGPUBuffer(
+    gpuCarsUintArray,
+    GPUBufferUsage.STORAGE | GPUBufferUsage.READ | GPUBufferUsage.COPY_SRC
+  );
+
+  carsFloatResultsBuffer = createGPUBuffer(
+    gpuCarsFloatArray,
+    GPUBufferUsage.STORAGE | GPUBufferUsage.READ | GPUBufferUsage.COPY_SRC
+  );
+  carsUintResultsBuffer = createGPUBuffer(
+    gpuCarsUintArray,
+    GPUBufferUsage.STORAGE | GPUBufferUsage.READ | GPUBufferUsage.COPY_SRC
+  );
 }
 
 shaderModule = null;
@@ -140,19 +201,190 @@ function createShaderModule() {
     struct NodeCoordinates {
       nodeCoordinates: array<NodeCoordinate>,
     }
+
+    struct CarUint {
+      id: u32,
+      wayId: u32,
+      nodeId: u32,
+      speed: u32,
+    }
+
+    struct CarUints {
+      carUints: array<CarUint>,
+    }
+
+    struct CarFloat {
+      lat: f32,
+      lon: f32,
+    }
+
+    struct CarFloats {
+      carFloats: array<CarFloat>,
+    }
     
     @group(0) @binding(0) var<storage, read> ways : Ways;
-    @group(0) @binding(1) var<storage, read> nodeIds : NodeIds;
-    @group(0) @binding(2) var<storage, read> nodeCoordinates : NodeCoordinates;
+    @group(0) @binding(1) var<storage, read> connectionIds: array<u32>;
+    @group(0) @binding(2) var<storage, read> nodeIds : NodeIds;
+    @group(0) @binding(3) var<storage, read> nodeCoordinates : NodeCoordinates;
+    @group(0) @binding(4) var<storage, read> carUints : CarUints;
+    @group(0) @binding(5) var<storage, read> carFloats : CarFloats;
+    @group(0) @binding(6) var<storage, read_write> carsUintResults : CarUints;
+    @group(0) @binding(7) var<storage, read_write> carsFloatResults : CarFloats;
     
     @compute @workgroup_size(8)
     fn main(@builtin(global_invocation_id) global_id : vec3<u32>) {
       var index = global_id.x;
-      // use these just to autodetect layout
-      var way = ways.ways[index];
-      var nodesOffset = way.nodesOffset;
-      var nodeIds = nodeIds.nodeIds[nodesOffset];
-      var nodeCoordinates = nodeCoordinates.nodeCoordinates[nodesOffset];
+      var carUint = carUints.carUints[index];
+      var carFloat = carFloats.carFloats[index];
+      var way : Way;
+      var nodeId : NodeId;
+      var nodeCoordinate : NodeCoordinate;
+      var nodeIndex : u32;
+
+      for (var i = 0u; i < arrayLength(&ways.ways); i = i + 1u) {
+        if (ways.ways[i].id == carUint.wayId) {
+          way = ways.ways[i];
+          break;
+        }
+      }
+
+      for (var i = 0u; i < arrayLength(&nodeIds.nodeIds); i = i + 1u) {
+        if (nodeIds.nodeIds[i].id == carUint.nodeId) {
+          nodeId = nodeIds.nodeIds[i];
+          nodeCoordinate = nodeCoordinates.nodeCoordinates[i];
+          nodeIndex = i - way.nodesOffset;
+          break;
+        }
+      }
+
+      var currentWay = way;
+      var previousWay = way;
+      var positionInWay = nodeIndex;
+      var previousNodeId = nodeId;
+      var previousNodeCoordinate = nodeCoordinate;
+      var nextNodeId = previousNodeId;
+      var nextNodeCoordinate = previousNodeCoordinate;
+      var canSpeedUp = false;
+      for (var distanceToCheck = 0u; distanceToCheck < max(carUint.speed, 1); distanceToCheck = distanceToCheck + 1u) {
+        if (positionInWay + 1 > currentWay.nodesLength - 1) {
+          if (currentWay.connectionsLength == 0u) {
+            carsUintResults.carUints[index] = CarUint(0u, 0u, 0u, 0u);
+            carsFloatResults.carFloats[index] = CarFloat(0.0, 0.0);
+            return;
+          }
+
+          previousWay = currentWay;
+          var currentWayId = connectionIds[currentWay.connectionsOffset];
+
+          var foundCurrentWay = false;
+          for (var i = 0u; i < arrayLength(&ways.ways); i = i + 1u) {
+            if (ways.ways[i].id == currentWayId) {
+              currentWay = ways.ways[i];
+              foundCurrentWay = true;
+              break;
+            }
+          }
+
+          if (!foundCurrentWay) {
+            carsUintResults.carUints[index] = CarUint(0u, 0u, 0u, 0u);
+            carsFloatResults.carFloats[index] = CarFloat(0.0, 0.0);
+            return;
+          }
+
+          var enteringRoundabout = currentWay.isRoundabout == 1u && previousWay.isRoundabout == 0u;
+          if (enteringRoundabout) {
+            var previousSectionOfRoundabout = Way(0u, 0u, 0u, 0u, 0u, 0u);
+            for (var i = 0u; i < arrayLength(&ways.ways); i = i + 1u) {
+              if (ways.ways[i].isRoundabout == 1u && connectionIds[ways.ways[i].connectionsOffset] == currentWayId) {
+                previousSectionOfRoundabout = ways.ways[i];
+                break;
+              }
+            }
+
+            var isCarInRoundabout = false;
+            for (var i = 0u; i < arrayLength(&carUints.carUints); i = i + 1u) {
+              if (carUints.carUints[i].wayId == currentWay.id || carUints.carUints[i].wayId == previousSectionOfRoundabout.id) {
+                isCarInRoundabout = true;
+                break;
+              }
+            }
+
+            if (isCarInRoundabout) {
+              carsUintResults.carUints[index] = CarUint(carUint.id, previousNodeId.wayId, previousNodeId.id, 0);
+              carsFloatResults.carFloats[index] = CarFloat(previousNodeCoordinate.lat, previousNodeCoordinate.lon);
+              return;
+            }
+          }
+        }
+
+        nextNodeId = nodeIds.nodeIds[way.nodesOffset + positionInWay + 1];
+        nextNodeCoordinate = nodeCoordinates.nodeCoordinates[way.nodesOffset + positionInWay + 1];
+        var isCarOnNextNode = false;
+        for (var i = 0u; i < arrayLength(&carUints.carUints); i = i + 1u) {
+          if (carUints.carUints[i].nodeId == nextNodeId.id && carUints.carUints[i].wayId == currentWay.id) {
+            isCarOnNextNode = true;
+            break;
+          }
+        }
+
+        if (isCarOnNextNode) {
+          carsUintResults.carUints[index] = CarUint(carUint.id, previousNodeId.wayId, previousNodeId.id, distanceToCheck);
+          carsFloatResults.carFloats[index] = CarFloat(previousNodeCoordinate.lat, previousNodeCoordinate.lon);
+          return;
+        }
+
+        previousNodeId = nextNodeId;
+        previousNodeCoordinate = nextNodeCoordinate;
+        positionInWay = positionInWay + 1u;
+
+        if (distanceToCheck == max(carUint.speed, 1) - 1u && carUint.speed < 2 && carUint.speed > 0) {
+          if (positionInWay + 1 > currentWay.nodesLength - 1) {
+            if (currentWay.connectionsLength == 0u) {
+              break;
+            }
+
+            var currentWayId = connectionIds[currentWay.connectionsOffset];
+            positionInWay = 0;
+
+            var foundCurrentWay = false;
+            for (var i = 0u; i < arrayLength(&ways.ways); i = i + 1u) {
+              if (ways.ways[i].id == currentWayId) {
+                currentWay = ways.ways[i];
+                foundCurrentWay = true;
+                break;
+              }
+            }
+
+            if (!foundCurrentWay) {
+              break;
+            }
+
+            nextNodeId = nodeIds.nodeIds[way.nodesOffset + positionInWay + 1];
+            nextNodeCoordinate = nodeCoordinates.nodeCoordinates[way.nodesOffset + positionInWay + 1];
+
+            var isCarOnNextNode = false;
+            for (var i = 0u; i < arrayLength(&carUints.carUints); i = i + 1u) {
+              if (carUints.carUints[i].nodeId == nextNodeId.id) {
+                isCarOnNextNode = true;
+                break;
+              }
+            }
+
+            if (!isCarOnNextNode) {
+              canSpeedUp = true;
+            }
+          }
+        }
+      }
+
+      var speedUp = 0u;
+      if (canSpeedUp) {
+        speedUp = 1u;
+      }
+
+      carsUintResults.carUints[index] = CarUint(carUint.id, nextNodeId.wayId, nextNodeId.id, max(carUint.speed, 1) + speedUp);
+      carsFloatResults.carFloats[index] = CarFloat(nextNodeCoordinate.lat, nextNodeCoordinate.lon);
+      return;
     }`,
   });
 
@@ -162,10 +394,9 @@ function createShaderModule() {
 async function computePassGpu() {
   const device = await getDevice();
   initStaticBuffers();
-
-  // Pipeline setup
-
+  initCarsBuffers();
   createShaderModule();
+
   const computePipeline = device.createComputePipeline({
     layout: 'auto',
     compute: {
@@ -174,41 +405,70 @@ async function computePassGpu() {
     },
   });
 
+  const entries = [
+    waysBuffer,
+    connectionIdsBuffer,
+    nodeIdsBuffer,
+    nodeCoordsBuffer,
+    carsUintBuffer,
+    carsFloatBuffer,
+    carsUintResultsBuffer,
+    carsFloatResultsBuffer,
+  ].map((buffer, index) => ({
+    binding: index,
+    resource: {
+      buffer,
+    },
+  }));
   const bindGroup = device.createBindGroup({
     layout: computePipeline.getBindGroupLayout(0),
-    entries: [
-      {
-        binding: 0,
-        resource: {
-          buffer: waysBuffer,
-        },
-      },
-      {
-        binding: 1,
-        resource: {
-          buffer: nodeIdsBuffer,
-        },
-      },
-      {
-        binding: 2,
-        resource: {
-          buffer: nodeCoordsBuffer,
-        },
-      },
-    ],
+    entries,
   });
 
-  // Commands submission
-
   const commandEncoder = device.createCommandEncoder();
-
   const passEncoder = commandEncoder.beginComputePass();
   passEncoder.setPipeline(computePipeline);
   passEncoder.setBindGroup(0, bindGroup);
   passEncoder.dispatchWorkgroups(8);
   passEncoder.end();
 
+  // Get a GPU buffer for reading in an unmapped state.
+  const [gpuCarsFloatArray, gpuCarsUintArray] = buildGpuCarsArray();
+
+  const gpuCarUintReadBuffer = device.createBuffer({
+    size: gpuCarsUintArray.byteLength,
+    usage: GPUBufferUsage.COPY_DST | GPUBufferUsage.MAP_READ,
+  });
+  const gpuCarFloatReadBuffer = device.createBuffer({
+    size: gpuCarsFloatArray.byteLength,
+    usage: GPUBufferUsage.COPY_DST | GPUBufferUsage.MAP_READ,
+  });
+
+  // Encode commands for copying buffer to buffer.
+  commandEncoder.copyBufferToBuffer(
+    carsUintResultsBuffer /* source buffer */,
+    0 /* source offset */,
+    gpuCarUintReadBuffer /* destination buffer */,
+    0 /* destination offset */,
+    gpuCarsUintArray.byteLength /* size */
+  );
+  commandEncoder.copyBufferToBuffer(
+    carsFloatResultsBuffer /* source buffer */,
+    0 /* source offset */,
+    gpuCarFloatReadBuffer /* destination buffer */,
+    0 /* destination offset */,
+    gpuCarsFloatArray.byteLength /* size */
+  );
+
   // Submit GPU commands.
   const gpuCommands = commandEncoder.finish();
   device.queue.submit([gpuCommands]);
+
+  // Read buffer.
+  await gpuCarUintReadBuffer.mapAsync(GPUMapMode.READ);
+  await gpuCarFloatReadBuffer.mapAsync(GPUMapMode.READ);
+  const arrayBuffer = gpuCarUintReadBuffer.getMappedRange();
+  const arrayBufferFloat = gpuCarFloatReadBuffer.getMappedRange();
+  cars = buildCpuCarsArray(new Uint32Array(arrayBuffer), new Float32Array(arrayBufferFloat));
+  // convert back to cars
 }
